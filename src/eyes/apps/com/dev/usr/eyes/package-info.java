a237b829f412b4f856e39cf42f74642f76a6a90b
chagasbezerra091-code <chagasbezerra091@gmail.com>
2025-10-31 13:54:44 -0300

    Create package-info.java

---
+++ src/eyes/apps/com/dev/usr/eyes/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * Pacote: com.dev.usr.eyes
+ *
+ * Contém as classes principais do sistema de visão do Linus Neural Project.
+ * - eyes.java : captura e processa quadros de vídeo
+ * - ia.java   : inteligência artificial visual (reconhecimento)
+ * - lux.java  : controle de brilho e percepção luminosa
+ *
+ * Licenciado sob Apache 2.0 — 2025 Linus Neural Project
+ */
+package com.dev.usr.eyes;
